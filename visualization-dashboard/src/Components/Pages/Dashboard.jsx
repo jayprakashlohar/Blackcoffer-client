3949838c5eb85f3e65a2bee6463e4b8054671447
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-05-18 17:14:27 +0530

    Bar chart completed

--- visualization-dashboard/src/Components/Pages/Dashboard.jsx
+++ visualization-dashboard/src/Components/Pages/Dashboard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
+import axios from "axios";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/data?page=1&limit=100").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    }).catch((error) => console.log(error))
+  },[])
+
+
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
 };
 
 export { Dashboard };
